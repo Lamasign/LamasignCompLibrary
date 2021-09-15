8adf98e08f58bc166e7f0f41110da09b9e9b549c
malolebrin <malolebrin@gmail.com>
2021-09-15 14:45:27 +0200

    FEAT link component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,33 @@
 <template>
-  <div class="w-full flex justify-center items-center">
-    <BButton>BUTTON</BButton>
-    <BButton variant="warning">BUTTON</BButton>
-    <BButton variant="danger">BUTTON</BButton>
-    <BButton variant="success">BUTTON</BButton>
-    <BButton variant="promotion">BUTTON</BButton>
-    <BButton variant="primary">BUTTON</BButton>
+  <div>
+    <div class="w-full flex justify-around items-center">
+      <BButton>BUTTON</BButton>
+      <BButton variant="warning">BUTTON</BButton>
+      <BButton variant="danger">BUTTON</BButton>
+      <BButton variant="success">BUTTON</BButton>
+      <BButton variant="promotion">BUTTON</BButton>
+      <BButton variant="primary">BUTTON</BButton>
+    </div>
+
+    <div class="w-full flex justify-around items-center">
+      <BLink>Link</BLink>
+      <BLink variant="warning">Link</BLink>
+      <BLink variant="danger">Link</BLink>
+      <BLink variant="success">Link</BLink>
+      <BLink variant="promotion">Link</BLink>
+      <BLink variant="primary">Link</BLink>
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 import BButton from '@/components/Button/BButton.vue'
+import BLink from './Link/BLink.vue'
 
 export default defineComponent({
-  components: { BButton },
+  components: { BButton, BLink },
   name: 'App'
 })
 </script>

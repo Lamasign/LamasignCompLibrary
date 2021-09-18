499d6b2aa6c87f62a4599d1d0244e8e3e666ec15
malolebrin <malolebrin@gmail.com>
2021-09-18 19:49:51 +0200

    FEAT test Tag component

---
+++ tests/unit/BTag.spec.ts
@@ -0,0 +1,30 @@
+import BTag from '@/components/Tag/BTag.vue'
+import { TagVariantsArray } from '@/components/Tag/BTag.types'
+import { createWrapper } from './utils'
+
+describe('BTag.vue', () => {
+	it('renders the content of its default slot', () => {
+		expect.assertions(1)
+		const slotContent = 'tag'
+		const wrapper = createWrapper(BTag, {
+			slots: { default: slotContent }
+		})
+		expect(wrapper.text()).toMatch(slotContent)
+	})
+
+	it('renders with the appropriate classes based on the "variant" prop', () => {
+		expect.assertions(TagVariantsArray.length + 1)
+
+		const defaultWrapper = createWrapper(BTag)
+		expect(defaultWrapper.element).toMatchSnapshot()
+
+		TagVariantsArray.forEach(variant => {
+			const wrapperWithProps = createWrapper(BTag, {
+				propsData: {
+					variant
+				}
+			})
+			expect(wrapperWithProps.element).toMatchSnapshot()
+		})
+	})
+})

9a2f6897fc01851a04dcd46c3274963b29661093
malolebrin <malolebrin@gmail.com>
2021-09-18 19:23:45 +0200

    FEAT unit jest and test it with BButton tests

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  transformIgnorePatterns: ['/node_modules/'],
+  // collectCoverage: true,
+  // collectCoverageFrom: ["**/*.{js,ts,vue}", "!**/node_modules/**"],
+}
+

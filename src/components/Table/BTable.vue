8541e5621dd8f381fb833dad00441788f81fefef
malolebrin <malolebrin@gmail.com>
2021-09-21 17:23:08 +0200

    Init scoped slots Btable compo

---
+++ src/components/Table/BTable.vue
@@ -0,0 +1,39 @@
+<template>
+  <table class="shadow-lg bg-white">
+    <!-- <slot
+      :item="item"
+      :index="index"
+    /> -->
+    <BTableColumn label="id">
+      <template v-slot="{ item }">
+        {{ item.value }}
+      </template>
+    </BTableColumn>
+  </table>
+</template>
+
+// scoped slots pour binder sur les fiels des items slots de colonnes 
+// props des colonnes le field surlequel on bind, le label du field, isSortable
+<script lang="ts">
+import { computed, defineComponent } from '@vue/composition-api'
+import BTableColumn from '@/components/Table/BTableColumn.vue'
+
+export default defineComponent({
+  name: 'BTable',
+  components: {
+    BTableColumn,
+  },
+  props: {
+    data: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup(props) {
+    const tableData = computed(() => props.data)
+    return {
+      tableData,
+    }
+  },
+})
+</script>
---
+++ src/components/Table/BTableColumn.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <thead>
+      <tr>
+        <th class="bg-blue-100 border text-left px-8 py-4">{{ label }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="border px-8 py-4">
+          <slot :item="item">
+          </slot>
+        </td>
+      </tr>
+    </tbody>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'BTableColumn',
+  props: {
+    label: {
+      type: String,
+      required: false,
+    },
+    arrayData: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup(props) {
+    const data = ref(props.arrayData)
+    const item = {
+      value: false,
+      name: 'Ice cream sandwich',
+      calories: 237,
+      fat: 9.0,
+      carbs: 37,
+      protein: 4.3,
+      iron: '1%'
+    }
+    return {
+      data,
+      item,
+    }
+  },
+})
+</script>

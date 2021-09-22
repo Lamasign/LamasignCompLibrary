caa8755fcc363276a8abf6deb2ca7e8a03822b22
malolebrin <malolebrin@gmail.com>
2021-09-23 00:38:10 +0200

    FEAT Switch comp + tests

---
+++ tests/unit/BSwitch.spec.ts
@@ -0,0 +1,35 @@
+import { createWrapper } from "./utils"
+import BSwitch from '@/components/Switch/BSwitch.vue'
+import { SwitchVariantsArray } from "@/components/Switch/BSwitch.types"
+
+describe('BSwitch.vue', () => {
+	it('renders correct variant based on variant props', () => {
+		expect.assertions(SwitchVariantsArray.length + 1)
+		const defaultWrapper = createWrapper(BSwitch)
+
+		expect(defaultWrapper.element).toMatchSnapshot()
+
+		SwitchVariantsArray.forEach(variant => {
+			const wrapperWithProps = createWrapper(BSwitch, {
+				propsData: {
+					variant
+				}
+			})
+			expect(wrapperWithProps.element).toMatchSnapshot()
+		})
+	})
+
+	it('Render correct value for props label ', async () => {
+		const label = 'test Label'
+		const wrapper = createWrapper(BSwitch)
+		await wrapper.setProps({ label })
+		expect(wrapper.props().label).toEqual(label)
+	})
+
+	it('emits a click "event" when the BSwitch is clicked', async () => {
+		const wrapper = createWrapper(BSwitch)
+		await wrapper.find('input').trigger('click')
+		expect(wrapper.emitted().switch).toBeTruthy()
+	})
+
+})

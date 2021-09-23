54526f842a2a7a417ed6fccc197c15aed2d506f4
malolebrin <malolebrin@gmail.com>
2021-09-23 14:22:52 +0200

    TESTS radio comp

---
+++ tests/unit/BRadio.spec.ts
@@ -0,0 +1,25 @@
+import BRadio from '@/components/Radio/BRadio.vue'
+import { Wrapper } from '@vue/test-utils'
+import { createWrapper } from './utils'
+
+describe('Bradio.vue', () => {
+	let wrapper: Wrapper<Vue> | null = null
+	const option = 'test option'
+
+	beforeEach(() => {
+		wrapper = createWrapper(BRadio, {
+			propsData: {
+				option,
+			}
+		})
+	})
+	it('render correct string passed on option prop', () => {
+		expect(wrapper!.props().option).toEqual(option)
+
+
+	})
+	it('emits a changeinput" when the BRadio is change', async () => {
+		await wrapper!.find('input').trigger('change')
+		expect(wrapper!.emitted().input).toBeTruthy()
+	})
+})

d4aa94f61a5a51dad6d9686d7cb9e05d8a23afd1
malolebrin <malolebrin@gmail.com>
2021-09-24 09:15:44 +0200

    REorder imoprts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,45 @@
-export * from '@/components/accordion/BAccordion.vue'
-export * from '@/components/Button/BButton.vue'
-export * from '@/components/CardModal/BCardModal.vue'
-export * from '@/components/Checkbox/BCheckbox.vue'
-export * from '@/components/Dropdown/BDropdown.vue'
-export * from '@/components/Field/BField.vue'
-export * from '@/components/Input/BInput.vue'
-export * from '@/components/Link/BLink.vue'
-export * from '@/components/Message/BMessage.vue'
-export * from '@/components/NumberInput/BNumberInput.vue'
-export * from '@/components/Table/BTable.vue'
-export * from '@/components/Tag/BTag.vue'
-export * from '@/components/Toast/BToast.vue'
-export * from '@/components/Table/BTable.vue'
-export * from '@/components/Table/BTableColumn.vue'
+import BAccordion from '@/components/accordion/BAccordion.vue'
+import BBadge from '@/components/Badge/BBadge.vue'
+import BButton from '@/components/Button/BButton.vue'
+import BCardModal from '@/components/CardModal/BCardModal.vue'
+import BCheckbox from '@/components/Checkbox/BCheckbox.vue'
+import BDropdown from '@/components/Dropdown/BDropdown.vue'
+import BField from '@/components/Field/BField.vue'
+import BFileInput from '@/components/FileInput/BFileInput.vue'
+import BInput from '@/components/Input/BInput.vue'
+import BLink from '@/components/Link/BLink.vue'
+import BMessage from '@/components/Message/BMessage.vue'
+import BNumberInput from '@/components/NumberInput/BNumberInput.vue'
+import BRadio from '@/components/Radio/BRadio.vue'
+import BSelect from '@/components/Select/BSelect.vue'
+import BSkeleton from '@/components/Skeleton/BSkeleton.vue'
+import BSwitch from '@/components/Switch/BSwitch.vue'
+import BTable from '@/components/Table/BTable.vue'
+import BTableColumn from '@/components/Table/BTableColumn.vue'
+import BTag from '@/components/Tag/BTag.vue'
+import BToast from '@/components/Toast/BToast.vue'
+import BTooltip from '@/components/Tooltip/BTooltip.vue'
+
+export {
+	BAccordion,
+	BBadge,
+	BButton,
+	BCardModal,
+	BCheckbox,
+	BDropdown,
+	BField,
+	BFileInput,
+	BInput,
+	BLink,
+	BMessage,
+	BNumberInput,
+	BRadio,
+	BSelect,
+	BSkeleton,
+	BSwitch,
+	BTable,
+	BTableColumn,
+	BTag,
+	BToast,
+	BTooltip,
+}

ad586025bf685aa5d7e725afe10456dd3d7d4f6d
malolebrin <malolebrin@gmail.com>
2021-10-01 10:10:36 +0200

    Export all types in types.ts

---
+++ src/types.ts
@@ -0,0 +1,241 @@
+import {
+	AccordionVariantsArray,
+	AccordionVariantsEnum,
+	AccordionVariantsMap,
+} from "./components/accordion/BAccordion.types"
+
+
+import {
+	BadgeColorsArray,
+	BadgeColorsEnum,
+	BadgeColorsMap,
+	BadgeSizeEnum,
+	BadgeSizesArray,
+	BadgeSizesMap,
+	BadgeVariantsArray,
+	BadgeVariantsEnum,
+	BadgeVariantsMap,
+} from '@/components/Badge/BBadge.types'
+
+import {
+	ButtonSizesEnum,
+	ButtonVariantsEnum,
+	ButtonTagsEnum,
+	ButtonTypesEnum,
+	ButtonSizesArray,
+	ButtonSizesMap,
+	ButtonVariantsArray,
+	ButtonVariantsMap,
+	ButtonTagsArray,
+	ButtonTypesArray,
+} from '@/components/Button/BButton.types'
+
+import {
+	CheckboxVariantsEnum,
+	CheckboxVariantsArray,
+	CheckboxsVariantsMap,
+} from '@/components/Checkbox/BCheckbox.types'
+
+import {
+	DropdownVariantsEnum,
+	DropdownDirectionEnum,
+	DropdownDirectionMap,
+	DropdownVariantsArray,
+	DropdownVariantsMap,
+} from '@/components/Dropdown/BDropdown.types'
+
+import {
+	FIELD_STATUS_INJECTION_KEY,
+	FieldStatusesArray,
+	FieldStatusesEnum,
+	FieldStatusesClassesMap,
+	FieldStatusesTypes,
+	FieldMessageStatusesClassesMap
+} from '@/components/Field/BField.types'
+
+import {
+	InputFileStatusesArray,
+	InputFileStatusesClassesMap,
+	InputFileStatusesEnum
+} from '@/components/FileInput/BFileInput.types'
+
+import {
+	InputTypesEnum,
+	InputTypesArray,
+	InputStatusesArray,
+	InputStatusesClassesMap,
+	InputStatusesEnum,
+	InputStatusesTypes,
+	InputTypes,
+} from '@/components/Input/BInput.types'
+
+import {
+	LinkDisabledClassesMap,
+	LinkTags,
+	LinkTagsArray,
+	LinkTagsEnum,
+	LinkVariantsArray,
+	LinkVariantsEnum,
+	LinkVariantsMap,
+} from '@/components/Link/BLink.types'
+
+import {
+	MessageVariantsArray,
+	MessageVariantsEnum,
+	MessageVariantsMap,
+} from '@/components/Message/BMessage.types'
+
+import {
+	NumberInputStatusesArray,
+	NumberInputStatusesClassesMap,
+	NumberInputStatusesEnum,
+	NumberInputStatusesTypes,
+} from '@/components/NumberInput/BNumberInput.types'
+
+import {
+	SelectColorsEnum,
+	SelectColorsArray,
+	SelectColorsMap,
+} from '@/components/Select/BSelect.types'
+
+import {
+	SkeletonVariantsEnum,
+	SkeletonVariantsArray,
+	SkeletonVariantsMap,
+	SkeletonAnimationsArray,
+	SkeletonAnimationsEnum,
+	SkeletonAnimationsMap,
+	SkeletonColorsArray,
+	SkeletonColorsEnum,
+	SkeletonColorsMap,
+} from '@/components/Skeleton/BSkeleton.types'
+
+import {
+	SwitchVariantsArray,
+	SwitchVariantsEnum,
+	SwitchVariantsMap,
+} from '@/components/Switch/BSwitch.types'
+
+import {
+	TableColumnHeaderVariantsMap,
+	TableColumnVariantsArray,
+	TableColumnVariantsEnum,
+	TableColumnVariantsMap,
+} from '@/components/Table/BTable.types'
+
+import {
+	TagVariantsArray,
+	TagVariantsEnum,
+	TagVariantsMap,
+	TagsVariantsMap,
+} from '@/components/Tag/BTag.types'
+
+import {
+	ToastVariantsArray,
+	ToastVariantsMap,
+	ToastVariantsEnum,
+} from '@/components/Toast/BToast.types'
+
+import {
+	TooltipPositionEnum,
+	TooltipPositionsArray,
+	TooltipPositionsMap,
+	TooltipVariantsArray,
+	TooltipVariantsEnum,
+	TooltipVariantsMap,
+} from '@/components/Tooltip/BTooltip.types'
+
+export {
+	AccordionVariantsArray,
+	AccordionVariantsEnum,
+	AccordionVariantsMap,
+	BadgeColorsArray,
+	BadgeColorsEnum,
+	BadgeColorsMap,
+	BadgeSizeEnum,
+	BadgeSizesArray,
+	BadgeSizesMap,
+	BadgeVariantsArray,
+	BadgeVariantsEnum,
+	BadgeVariantsMap,
+	ButtonSizesEnum,
+	ButtonVariantsEnum,
+	ButtonTagsEnum,
+	ButtonTypesEnum,
+	ButtonSizesArray,
+	ButtonSizesMap,
+	ButtonVariantsArray,
+	ButtonVariantsMap,
+	ButtonTagsArray,
+	ButtonTypesArray,
+	CheckboxVariantsEnum,
+	CheckboxVariantsArray,
+	CheckboxsVariantsMap,
+	DropdownVariantsEnum,
+	DropdownDirectionEnum,
+	DropdownDirectionMap,
+	DropdownVariantsArray,
+	DropdownVariantsMap,
+	FIELD_STATUS_INJECTION_KEY,
+	FieldStatusesArray,
+	FieldStatusesEnum,
+	FieldStatusesClassesMap,
+	FieldStatusesTypes,
+	FieldMessageStatusesClassesMap,
+	InputFileStatusesArray,
+	InputFileStatusesClassesMap,
+	InputFileStatusesEnum,
+	InputTypesEnum,
+	InputTypesArray,
+	InputStatusesArray,
+	InputStatusesClassesMap,
+	InputStatusesEnum,
+	InputStatusesTypes,
+	InputTypes,
+	LinkDisabledClassesMap,
+	LinkTags,
+	LinkTagsArray,
+	LinkTagsEnum,
+	LinkVariantsArray,
+	LinkVariantsEnum,
+	LinkVariantsMap,
+	MessageVariantsArray,
+	MessageVariantsEnum,
+	MessageVariantsMap,
+	NumberInputStatusesArray,
+	NumberInputStatusesClassesMap,
+	NumberInputStatusesEnum,
+	NumberInputStatusesTypes,
+	SelectColorsEnum,
+	SelectColorsArray,
+	SelectColorsMap,
+	SkeletonVariantsEnum,
+	SkeletonVariantsArray,
+	SkeletonVariantsMap,
+	SkeletonAnimationsArray,
+	SkeletonAnimationsEnum,
+	SkeletonAnimationsMap,
+	SkeletonColorsArray,
+	SkeletonColorsEnum,
+	SkeletonColorsMap,
+	SwitchVariantsArray,
+	SwitchVariantsEnum,
+	SwitchVariantsMap,
+	TableColumnHeaderVariantsMap,
+	TableColumnVariantsArray,
+	TableColumnVariantsEnum,
+	TableColumnVariantsMap,
+	TagVariantsArray,
+	TagVariantsEnum,
+	TagVariantsMap,
+	TagsVariantsMap,
+	ToastVariantsArray,
+	ToastVariantsMap,
+	ToastVariantsEnum,
+	TooltipPositionEnum,
+	TooltipPositionsArray,
+	TooltipPositionsMap,
+	TooltipVariantsArray,
+	TooltipVariantsEnum,
+	TooltipVariantsMap,
+}

8a290c3ae821d1660847d8f5e3ffa0291a4034c3
malolebrin <malolebrin@gmail.com>
2021-10-04 15:45:07 +0200

    Revert "FIX errro build"
    This reverts commit 628989ed67ea824add95f31e412ad4f56a5b0cc0.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import BAccordion from './accordion/BAccordion.vue'
 import BBadge from './Badge/BBadge.vue'
 import BButton from './Button/BButton.vue'
@@ -19,6 +20,29 @@ import BTableColumn from './Table/BTableColumn.vue'
 import BTag from './Tag/BTag.vue'
 import BToast from './Toast/BToast.vue'
 import BTooltip from './Tooltip/BTooltip.vue'
+=======
+import { default as BAccordion } from './components/accordion/BAccordion.vue'
+import { default as BBadge } from './components/Badge/BBadge.vue'
+import { default as BButton } from './components/Button/BButton.vue'
+import { default as BCardModal } from './components/CardModal/BCardModal.vue'
+import { default as BCheckbox } from './components/Checkbox/BCheckbox.vue'
+import { default as BDropdown } from './components/Dropdown/BDropdown.vue'
+import { default as BField } from './components/Field/BField.vue'
+import { default as BFileInput } from './components/FileInput/BFileInput.vue'
+import { default as BInput } from './components/Input/BInput.vue'
+import { default as BLink } from './components/Link/BLink.vue'
+import { default as BMessage } from './components/Message/BMessage.vue'
+import { default as BNumberInput } from './components/NumberInput/BNumberInput.vue'
+import { default as BRadio } from './components/Radio/BRadio.vue'
+import { default as BSelect } from './components/Select/BSelect.vue'
+import { default as BSkeleton } from './components/Skeleton/BSkeleton.vue'
+import { default as BSwitch } from './components/Switch/BSwitch.vue'
+import { default as BTable } from './components/Table/BTable.vue'
+import { default as BTableColumn } from './components/Table/BTableColumn.vue'
+import { default as BTag } from './components/Tag/BTag.vue'
+import { default as BToast } from './components/Toast/BToast.vue'
+import { default as BTooltip } from './components/Tooltip/BTooltip.vue'
+>>>>>>> parent of 628989e (FIX errro build)
 
 export {
 	BAccordion,
